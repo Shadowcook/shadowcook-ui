cb174b4518fd8adb69f02e165e5f70aaf174c3b3
Mitch Haraldsson <dev@shadowsoft.de>
2025-06-24 00:30:05 +0200

    Added version info with tag

--- frontend/src/layouts/elements/DefaultFooter.tsx
+++ frontend/src/layouts/elements/DefaultFooter.tsx
@@ -1,4 +1,5 @@
 export function DefaultFooter() {
+    const version = import.meta.env.VITE_BUILD_TAG;
     return (
         <>
             <div className="footerLeft">
@@ -7,7 +8,7 @@ export function DefaultFooter() {
             <div className="footerRight">
                 <a href="/" className="footerLink">HOME</a>&nbsp;|&nbsp;
                 <a href="/legal/dataprotection" className="footerLink">Data protection</a>&nbsp;|&nbsp;
-                UI Version 1.0
+                UI Version {version}
 
             </div>
         </>
